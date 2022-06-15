9823aafa7978cfe108fbaf44656bf7fb5f9f90b9
Leo Zhang <86931572+leoz0214@users.noreply.github.com>
2022-06-15 21:52:06 +0100

    Delete Language Translator Website directory

--- Language Translator Website/static/savedtranslations.js
+++
@@ -1,5 +0,0 @@
-function deleteAllSavedTranslations() {
-    if (confirm("Are you sure you would like to delete ALL of your saved translations? This is a big action and cannot be undone.")) {
-        window.location = "/delete-all-saved-translations";
-    }
-}
